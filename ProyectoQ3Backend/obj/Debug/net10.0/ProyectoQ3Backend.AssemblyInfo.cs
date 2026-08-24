7fb63b5c91e5b4aac5c1c48ce744892cad7883bf
Your Name <djcacontreras@gmail.com>
2026-08-24 14:37:36 -0600

    NoteServices Completed

--- ProyectoQ3Backend/obj/Debug/net10.0/ProyectoQ3Backend.AssemblyInfo.cs
+++ ProyectoQ3Backend/obj/Debug/net10.0/ProyectoQ3Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoQ3Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d51be1a72dfd1d888870ca37540fc998ae72dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205a8cb35111aa4249353c4ff6132324a32ad957")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoQ3Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoQ3Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
